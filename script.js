37d2e689b5466e349b87acb39ddf0cb22832face
vishnudattasvd <vishnudatta@gmail.com>
2025-01-19 22:55:12 +0530

    Minor Changes in task bar and about me section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["Backend Developer", "YouTuber", "Tester"],
+    strings: ["Backend Developer", "Software Test Engineer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
